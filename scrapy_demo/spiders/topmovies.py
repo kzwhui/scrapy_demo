9c646cef0d405f3812e54ed4ff1f38de5d393393
kzwhui@gmail.com <kzwhui@gmail.com>
2016-11-08 16:46:55 +0800

    crawl top 250 movies

--- scrapy_demo/spiders/topmovies.py
+++ scrapy_demo/spiders/topmovies.py
@@ -9,10 +9,11 @@ from scrapy_demo.items import ScrapyDemoItem
 class TopmoviesSpider(scrapy.Spider):
     name = "topmovies"
     allowed_domains = ["movie.douban.com"]
-    start_urls = ['https://movie.douban.com/top250/']
 
     def start_requests(self):
-        for url in self.start_urls:
+        url_template = 'https://movie.douban.com/top250?start=%s'
+        for i in range(0, 250, 25):
+            url = url_template % i
             hostname = urlparse.urlparse(url).netloc
             header={'Host':hostname, 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, likeGecko) Chrome/53.0.2785.143 Safari/537.36'}
             yield scrapy.Request(url=url, callback=self.parse, headers=header)
